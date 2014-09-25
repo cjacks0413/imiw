d43da24f016663f70f059ea818324f3e6f4d4971
cjacks0413 <chjackson93@gmail.com>
2014-09-24 21:04:23 -0400

    fixed geojson route bug;

--- map.js
+++ map.js
@@ -5,31 +5,7 @@ var map = L.mapbox.map('map', 'cjacks04.jij42jel')
 var svg = d3.select(map.getPanes().overlayPane).append("svg"),
     g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
-d3.json("data/03_Iraq.geojson", function(collection) {
-	var transform = d3.geo.transform({point: projectPoint}),
-	path = d3.geo.path().projection(transform);
-	var feature = g.selectAll("path")
-				   .data(collection.features)
-				   .enter().append("path");
-	
-	map.on("viewreset", reset);
-	reset();
-	
-	// Reposition the SVG to cover the features.
-	function reset() {
-		var bounds = path.bounds(collection),
-		topLeft = bounds[0],
-		bottomRight = bounds[1];
-		svg.attr("width", bottomRight[0] - topLeft[0])
-			.attr("height", bottomRight[1] - topLeft[1])
-			.style("left", topLeft[0] + "px")
-			.style("top", topLeft[1] + "px");
-		g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
-		feature.attr("d", path);
-	}
-
-
-}); 
+var iraq_routes = L.geoJson(iraqRoutes).addTo(map); 
 
 function projectPoint(x, y) {
 		var point = map.latLngToLayerPoint(new L.LatLng(y, x));
