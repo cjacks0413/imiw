57f28444baf2bcb45e794d661aaa4579ab8785b4
cjacks0413 <chjackson93@gmail.com>
2015-02-02 22:41:15 -0500

    fix g vs svg issue

--- graph.js
+++ graph.js
@@ -20,12 +20,9 @@ for (var i = 0; i < routes.length; i++) {
 	graph.addEdge(e, s); 
 	graph.getEdge(e, s)._eid = e;
 	graph.getEdge(e, s)._sid = s; 
+	graph.getEdge(e, s)._id = routes[i].properties.id; 
 }
 
-/* TESTING BFS 
-var a = graph.getNode("ABARQUH_532N311E_C06");
-var b = graph.getNode("QARYABIDH_531N306E_C06");
-bfs(graph, a, b);  */ 
 
 /* queue */ 
 function Queue() {
@@ -46,36 +43,57 @@ function Queue() {
 /* BFS 
  * s = start vertex
  * t = target vertex
+ * return path from s to t 
  */
 
 function bfs(G, s, t) {
+	var previous = {};
 	var queue = new Queue(); 
-	console.log(queue);
 	var target, u, v, edges; 
 	queue.enqueue(s);
+	previous[s._id] = null; 
 	s.visited = true; 
 	while(!queue.empty()) {
-		target = queue.dequeue(); 
-		target.visited = true; 
-		if (target._id == t._id) {
-			return true; //decide what to do here
+		//console.log(queue);
+		target = queue.dequeue();	
+		target.visited = true;
+		if (target._id === t._id) {
+			return extractPathFromPrevious(graph, previous, s, t);
 		}
 		var edges = G.getAllEdgesOf(target._id);
-		console.log(edges.length);
 		for (var i = 0; i < edges.length; i++) {
-			enqueueIfNotVisited(G, edges[i], queue);
+			enqueueIfNotVisited(G, edges[i], queue, target, previous);
 		}
 	}
+	return false; 
 }
 
-function enqueueIfNotVisited(G, edge, q){
+/* Given previous array, return the path from s to t by node id*/
+function extractPathFromPrevious(graph, previous, s, t) {
+	var path = new Array();
+	/* put target in path */
+	path.push(t._id);
+
+	var id = previous[t._id]._id; 
+	while(id != s._id) {
+		path.push(id);
+		id = previous[id]._id; 
+	}	
+	/* put source in path */
+	path.push(id);
+	return path; 
+}
+
+function enqueueIfNotVisited(G, edge, q, target, previous){
 	var u = G.getNode(edge._eid);
 	var v = G.getNode(edge._sid); 
 	if (u.visited === false) {
 		u.visited = true;
+		previous[u._id] = target; 
 		q.enqueue(u);
 	} if (v.visited === false) {
 		v.visited = true;
+		previous[v._id] = target;
 		q.enqueue(v);
 	}
 
--- map.js
+++ map.js
@@ -1,37 +1,47 @@
 var debug = true; 
+
 L.mapbox.accessToken = 'pk.eyJ1IjoiY2phY2tzMDQiLCJhIjoiVFNPTXNrOCJ9.k6TnctaSxIcFQJWZFg0CBA';
 var map = L.mapbox.map('map', 'cjacks04.jij42jel')
     .setView([31, 35], 4);
 
+
 /*------------------------------------------------------
- * SETUP SITES AND ROUTES
+ * SETUP SITES 
  *------------------------------------------------------*/
+
+/* note to self: 
+ * svg: appending svg appends <svg/> to an element so you can draw all the shapes etc. 
+ * g : appending g to svg allows you to group multiple svg shapes, so you can transform them all as if 
+ *     they were a single shape. 
+ */ 
 map._initPathRoot()
-var svg = d3.select("#map").select("svg"),
-g = svg.append("g"); 
+var svg = d3.select("#map").select("svg");
+g = svg.append("g")
 
 
-var svgContainer = d3.select(map.getPanes().overlayPane).append("svg"); 
-//g maybe? 
-var group = svgContainer.append("g").attr("class", "leaflet-zoom-hide")
-var width = document.getElementById('container').offsetWidth;
-var height = width / 2;
+/* SITES */
 
 var sites = places.data; 
+var test = [];
+test.push(sites[2250]);
+test.push(sites[50]);
 /* Add a LatLng object to each item in the dataset */
-
 sites.forEach(function(d) {
 	d.LatLng = new L.LatLng(d.lat, d.lon);
 })
 
 var svgSites = g.selectAll("circle")
 	.data(sites)
-	.enter().append("circle")
+	.enter()
+	.append("circle")
 	.attr("class", "node")
-	.attr("r", 5);
+	.attr("r", 5)
+	.on("click", popup); 
 
 map.on("viewreset", update);
+
 update();
+
 function update() {
 	svgSites.attr("transform",
 		function(d) {
@@ -40,15 +50,25 @@ function update() {
 			map.latLngToLayerPoint(d.LatLng).y +")";
 		}
 	)
+
 }
 
-/* TODO: MAKE PATH INTERACTIVE..? */
-var group = svgContainer.append("g").attr("class", "leaflet-zoom-hide")
-var path = d3.geo.path().projection(project); 
+/* TODO: TOOLTIP */
 
-// var projection = d3.geo.mercator()
-// 				 .scale(1000)
-// 				 .translate(width/2, height/2); 
+function popup(d) {
+
+}
+
+
+/*------------------------------------------------------
+ * SETUP ROUTES
+ *------------------------------------------------------*/
+var routes = allRoutes.features, 
+    path = d3.geo.path().projection(project), 
+    bounds = d3.geo.bounds(allRoutes),
+    svgContainer = d3.select(map.getPanes().overlayPane).append("g"),
+    group = svgContainer.append("g").attr("class", "leaflet-zoom-hide"),
+    feature;
 
 function project(point) {
 	var latlng = new L.LatLng(point[1], point[0]); 
@@ -56,15 +76,32 @@ function project(point) {
 	return [layerPoint.x, layerPoint.y]; 
 }
 
-d3.json("data/all_routes.json", function(routes) {
-	var feature = group.selectAll("path")
-	   .data(routes.features)
-	   .enter()
-	   .append("path");
+/* show subset of allRoutes */
+function showPath(partial) {
+	feature = g.selectAll("path")
+	.data(partial)
+	.enter()
+	.append("path");
 
-	var bounds = d3.geo.bounds(routes); 
+	resetMap();
+	map.on("viewset", resetMap);
+}
 
-	function reset() {
+/* Show all paths */
+function showAllPaths() {
+	d3.json("data/all_routes.json", function(routes) {
+		feature = g.selectAll("path")
+		   .data(routes.features)
+		   .enter()
+		   .append("path");
+
+	 	resetMap(); 
+	 	map.on("viewreset", resetMap); 
+	})  
+}
+
+function resetMap() {
+		/* reposition paths*/
 	    var bottomLeft = project(bounds[0]),
 	    topRight = project(bounds[1]);
 	 
@@ -74,14 +111,92 @@ d3.json("data/all_routes.json", function(routes) {
 	         .style("margin-top", topRight[1] + "px");
 	 
 	    group.attr("transform", "translate(" + -bottomLeft[0] + "," + -topRight[1] + ")");
-	 
+
 	    feature.attr("d", path);
+
+	    /* reposition sites*/
+	    svgSites.attr("transform",
+			function(d) {
+				return "translate("+
+				map.latLngToLayerPoint(d.LatLng).x +","+
+				map.latLngToLayerPoint(d.LatLng).y +")";
+		});
  	}
 
- 	reset(); 
- 	map.on("viewreset", reset); 
 
-})
+
+/* ----------------------------------------------------
+ * PATH FROM A TO B 
+ * Given a start and end node ID, shows path on path
+ * TODO: deal with the missing pieces ...
+ * TODO: animate the path  
+ * ----------------------------------------------------*/
+var routesByEID = {}, 
+    pathFromAToB; 
+
+
+/* Tester functions. TODO: add showAllPaths as a layer*/
+drawPathFromSourceToTarget("BAGHDAD_443N333E_C03","YATHRIB_396N244E_C07" );
+showAllPaths();
+
+function drawPathFromSourceToTarget(sid, tid) {
+	var s, t, sTot; 
+	sortRoutesByEID(); 
+	s = graph.getNode(sid);
+	t = graph.getNode(tid);
+	sTot = bfs(graph, s, t); 
+ 	pathFromAToB = sTot; 
+	topoPath = createTopoPath(sTot); 
+	showPath(topoPath);
+}
+
+
+function sortRoutesByEID() {
+	var r; 
+	for (var i = 0; i < routes.length; i++) {
+		r = routes[i].properties.eToponym;
+		if (routesByEID[r] === undefined) {
+			routesByEID[r] = routes[i];
+		}
+	}
+}
+
+function createTopoPath(path) {
+	var topoPath = []; 
+	var routeSection; 
+	for (var i = 0; i < path.length; i++ ) {
+		routeSection = routesByEID[path[i]];
+		topoPath.push(routeSection);
+	}
+	topoPath = topoPath.filter(isPartOfRoute);
+	return topoPath; 
+}
+
+
+function isPartOfRoute(element, index, array) {
+	return ((pathFromAToB.indexOf(element.properties.eToponym) >= 0 ) && 
+		   (pathFromAToB.indexOf(element.properties.sToponym) >= 0))  
+}
+
+
+
+/*--------------------------------------------------------
+ * UTIL 
+ *-------------------------------------------------------*/
+function sortSitesByTopURI() {
+	var sitesToAdd = places.data; 
+	var sortedSites = {}; 
+	for (var i = 0; i < sitesToAdd.length; i++) {
+		if (sites[sitesToAdd[i].topURI] === undefined) {
+			sites[sitesToAdd[i].topURI] = sitesToAdd[i]; 
+		}
+	}
+	return sortedSites; 
+}
+ 
+
+
+
 
 
 
