8157706ee268ce2c63ed534ef0aa334967dc87a7
cjacks0413 <chjackson93@gmail.com>
2015-03-10 00:28:54 -0400

    preliminary ruby code for estimate

--- estimate.rb
+++ estimate.rb
@@ -1,15 +1,34 @@
 require 'json'
 
 routes = File.read('all_routes.json'); 
-input = CSV.read(ARGV[1]);
+routes_hash = JSON.parse(routes); 
+features = routes_hash["features"];
+proportions = new Array(); 
 
-sites_hash = JSON.parse(cornu); 
+CSV.foreach(ARGV[0]) do |site| 
+	distSource = site[0]
+	s = site[1]
+	e = site[2]
+	distMeters = findDistanceInMeters(s, e); 
+	if distMeters > 0 
+		proportions.push(distMeters / distSource)
+	end 
+end 
 
-# foreach line in input 
-# if path exist from input[i][1] - input[i][2], 
-# proportion = meters / total 
+# sum all the numbers
+average = proportions.reduce(:+)
 
-# output: average meter / total ratio 
+puts average; 
+
+def findDistanceInMeters(s, e)
+	features['children'].each do |c|
+		puts c['properties']['eToponym']
+		if c['properties']['eToponym'] == s && c['properties']['sToponyms'] == e 
+			return c['properties']['Meter'];
+		else return 0 
+		end 
+	end 
+end 
 
 
 
